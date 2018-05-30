a1d835fc8bc6db8ded7df7febb6f9ec5060848c9
Eduardo Caldas <eduucaldas@gmail.com>
2018-05-30 11:19:22 +0200

    new verifier

--- verify_complete.cpp
+++ verify_complete.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
+/*
+We want to test if all nodes are in the scc, or if there is one node that
+appears twice
+*/
+int main() {
+  int n;
+  map<int, bool> quant;
+  vector<int> repeated;
+  vector<int> missing;
+  while (cin >> n) {
+    if (quant[n] == false)
+      quant[n] = true;
+    else
+      repeated.push_back(n);
+  }
+  int i = 0;
+  for (map<int, bool>::iterator it = quant.begin(); it != quant.end();
+       it++, i++) {
+    if (i != it->first) {
+      for (; i < it->first; i++) missing.push_back(i);
+    }
+  }
+  if (!repeated.empty()) {
+    cout << "repeated vertices" << endl;
+    for (std::vector<int>::iterator i = repeated.begin(); i != repeated.end();
+         ++i) {
+      cout << *i << " ";
+    }
+  }
+  cout << endl;
+  if (!missing.empty()) {
+    cout << "missing vertices" << endl;
+    for (std::vector<int>::iterator i = missing.begin(); i != missing.end();
+         ++i) {
+      cout << *i << " ";
+    }
+  }
+  cout << endl;
+  if (missing.empty() && repeated.empty()) cout << "It seems Ok";
 
-int main(){
-	int n;
-	map<int, int> quant;
-	while (cin >> n){
-		quant[n]++;
-	}
-	int i = 0;
-	for(map<int, int>::iterator it = quant.begin(); it != quant.end(); it++, i++){
-		if (i != it->first)
-			cout << "WARNING ";
-		cout << it->first << " " << it->second << endl;
-	}
-	return 0;
+  return 0;
 }
