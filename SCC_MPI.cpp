e9aa9ee786ef33195f13c5d0712be3a94722ea09
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2018-05-30 12:42:22 +0200

    optimizing succesors and predecessors

--- SCC_MPI.cpp
+++ SCC_MPI.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <fstream>
 #include <mpi.h>
+#include <ctime>
 
 using namespace std;
 
@@ -47,7 +48,7 @@ vector<unordered_set<int> > generateEdgesIn(vector<unordered_set<int> >& graph_e
 	return graph_edges_in;
 }
 
-unordered_set<int> successors(vector<unordered_set<int> >& graph_edges_out, int start){
+unordered_set<int> successorsSeq(vector<unordered_set<int> >& graph_edges_out, int start){
 	unordered_set<int> succ;
 	queue<int> toVisit;
 	int node;
@@ -70,7 +71,7 @@ unordered_set<int> successors(vector<unordered_set<int> >& graph_edges_out, int 
 	return succ;
 }
 
-unordered_set<int> predecessors(vector<unordered_set<int> >& graph_edges_in, int start){
+unordered_set<int> predecessorsSeq(vector<unordered_set<int> >& graph_edges_in, int start){
 	unordered_set<int> pred;
 	queue<int> toVisit;
 	int node;
@@ -106,8 +107,8 @@ void DCSC_Sequential(vector<int> vertices, vector<unordered_set<int> >& graph_ed
 
 	int start = vertices[rand() % vertices.size()];
 
-	unordered_set<int> succ = successors(graph_edges_out, start);
-	unordered_set<int> pred = predecessors(graph_edges_in, start);
+	unordered_set<int> succ = successorsSeq(graph_edges_out, start);
+	unordered_set<int> pred = predecessorsSeq(graph_edges_in, start);
 
 	vector<int> s1, s2, s3, s4;
 	for(int i = 0; i < vertices.size(); i++){
@@ -129,6 +130,52 @@ void DCSC_Sequential(vector<int> vertices, vector<unordered_set<int> >& graph_ed
 	DCSC_Sequential(s3, graph_edges_out, graph_edges_in, scc);
 	DCSC_Sequential(s4, graph_edges_out, graph_edges_in, scc);
 
+}
+
+unordered_set<int> successors(unordered_set<int>& vertices, vector<unordered_set<int> >& graph_edges_out, int start){
+	unordered_set<int> succ;
+	queue<int> toVisit;
+	int node;
+	
+	succ.insert(start);
+	toVisit.push(start);
+
+	while(!toVisit.empty()){
+		node = toVisit.front();
+		toVisit.pop();
+
+		for(unordered_set<int>::iterator it = graph_edges_out[node].begin(); it != graph_edges_out[node].end(); it++){
+			if (succ.count(*it) == 0 && vertices.count(*it) > 0){
+				succ.insert(*it);
+				toVisit.push(*it);
+			}
+		}
+	}
+
+	return succ;
+}
+
+unordered_set<int> predecessors(unordered_set<int>& vertices, vector<unordered_set<int> >& graph_edges_in, int start){
+	unordered_set<int> pred;
+	queue<int> toVisit;
+	int node;
+	
+	pred.insert(start);
+	toVisit.push(start);
+
+	while(!toVisit.empty()){
+		node = toVisit.front();
+		toVisit.pop();
+
+		for(unordered_set<int>::iterator it = graph_edges_in[node].begin(); it != graph_edges_in[node].end(); it++){
+			if (pred.count(*it) == 0 && vertices.count(*it) > 0){
+				pred.insert(*it);
+				toVisit.push(*it);
+			}
+		}
+	}
+
+	return pred;
 }
 
 void DCSC_Rec(vector<int> vertices, vector<unordered_set<int> >& graph_edges_out, vector<unordered_set<int> >& graph_edges_in){
@@ -155,8 +202,10 @@ void DCSC_Rec(vector<int> vertices, vector<unordered_set<int> >& graph_edges_out
 
 	int start = vertices[rand() % n_vertices];
 
-	unordered_set<int> succ = successors(graph_edges_out, start);
-	unordered_set<int> pred = predecessors(graph_edges_in, start);
+	unordered_set<int> vertices_set(vertices.begin(), vertices.end());
+
+	unordered_set<int> succ = successors(vertices_set, graph_edges_out, start);
+	unordered_set<int> pred = predecessors(vertices_set, graph_edges_in, start);
 
 	// Find the four differents sets of verticex
 	vector<int> s1, s2, s3, s4;
@@ -173,27 +222,29 @@ void DCSC_Rec(vector<int> vertices, vector<unordered_set<int> >& graph_edges_out
 			s4.push_back(vertices[i]);
 	}
 
+	cout << "proc " << task_id << " finds s1 " << s1.size() << " | s2 " << s2.size() << " | s3 " << s3.size() << " | s4 " << s4.size() << endl;
+
 	// Send the connected component to the root
-	cout << "proc " << task_id << " sends a vector of size " << s1.size() << " and tag " << 2 << endl;
+	cout << "proc " << task_id << " sends a vector s1 of size " << s1.size() << " and tag " << 2 << endl;
 	MPI_Send(&s1[0], s1.size(), MPI_INT, 0, 2, MPI_COMM_WORLD);
 
 	// Send the set of vertices to the root so that it allocates it to another processor
 	if(s2.size() > 1){
-		cout << "proc " << task_id << " sends a vector of size " << s2.size() << " and tag " << 1 << endl;
+		cout << "proc " << task_id << " sends a vector s2 of size " << s2.size() << " and tag " << 1 << endl;
 		MPI_Send(&s2[0], s2.size(), MPI_INT, 0, 1, MPI_COMM_WORLD);
 	}
 	else if(s2.size() == 1){
-		cout << "proc " << task_id << " sends a vector of size " << s2.size() << " and tag " << 2 << endl;
+		cout << "proc " << task_id << " sends a vector s2 of size " << s2.size() << " and tag " << 2 << endl;
 		MPI_Send(&s2[0], s2.size(), MPI_INT, 0, 2, MPI_COMM_WORLD);
 	}
 	
 	// Send the set of vertices to the root so that it allocates it to another processor
 	if(s3.size() > 1){
-		cout << "proc " << task_id << " sends a vector of size " << s3.size() << " and tag " << 1 << endl;
+		cout << "proc " << task_id << " sends a vector s3 of size " << s3.size() << " and tag " << 1 << endl;
 		MPI_Send(&s3[0], s3.size(), MPI_INT, 0, 1, MPI_COMM_WORLD);
 	}
 	else if(s3.size() == 1){
-		cout << "proc " << task_id << " sends a vector of size " << s3.size() << " and tag " << 2 << endl;
+		cout << "proc " << task_id << " sends a vector s3 of size " << s3.size() << " and tag " << 2 << endl;
 		MPI_Send(&s3[0], s3.size(), MPI_INT, 0, 2, MPI_COMM_WORLD);
 	}
 	
